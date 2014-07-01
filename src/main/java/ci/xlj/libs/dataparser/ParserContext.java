ff87ea3f0a717f59f714a44d92f4d70cacebc184
kfzx-xulj <kfzx-xulj@KFZXXMPUB012.Intranet.ICBC.COM.CN>
2014-07-01 20:36:40 +0800

    initial import

---
+++ src/main/java/ci/xlj/libs/dataparser/ParserContext.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//      Contributors:      Xu Lijia 
+
+package ci.xlj.libs.dataparser;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import ci.xlj.libs.jenkinsvisitor.JenkinsVisitor;
+import ci.xlj.libs.dataparser.restapi.APIParser;
+
+public class ParserContext {
+
+	private static Logger logger = Logger.getLogger(ParserContext.class);
+
+	private static ParserContext instance;
+
+	private ParserContext() {
+	}
+
+	public static ParserContext getInstance() {
+		if (instance == null) {
+			instance = new ParserContext();
+		}
+
+		return instance;
+	}
+
+	/**
+	 * Parse data by api
+	 */
+	public String parseData(JenkinsVisitor visitor, String jobName,
+			String pluginUrlFragment, List<String> dataItems) {
+		if (dataItems.size() > 0) {
+			APIParser apiParser = new APIParser(visitor, jobName,
+					pluginUrlFragment);
+			return apiParser.parse(dataItems);
+		} else {
+			logger.error("No data items provided for " + jobName);
+			return "No data to be analyzed.";
+		}
+	}
+
+	/**
+	 * Parse data by plugin
+	 */
+	public String parseData(String pluginPath, String pluginClassName,
+			String reportFilePath) throws Exception {
+
+		File plugin = new File(pluginPath);
+
+		URLClassLoader ClassLoader = new URLClassLoader(new URL[] { plugin
+				.toURI().toURL() });
+
+		Class<?> pluginClass = ClassLoader.loadClass(pluginClassName);
+
+		Object pluginInstance = pluginClass.newInstance();
+
+		Method method = pluginClass.getMethod("parse");
+
+		return method.invoke(pluginInstance, reportFilePath).toString();
+	}
+}
---
+++ src/main/java/ci/xlj/libs/dataparser/restapi/APIParser.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//      Contributors:      Xu Lijia 
+
+package ci.xlj.libs.dataparser.restapi;
+
+import java.util.List;
+
+import net.sf.json.JSONObject;
+
+import org.apache.log4j.Logger;
+
+import ci.xlj.libs.jenkinsvisitor.JenkinsVisitor;
+import ci.xlj.libs.utils.StringUtils;
+import ci.xlj.libs.dataparser.Parsable;
+
+public class APIParser implements Parsable<List<String>> {
+
+	private static Logger logger = Logger.getLogger(APIParser.class);
+
+	private JSONObject json;
+
+	private StringBuilder data;
+
+	private String jobName;
+	private String pluginUrlFragment;
+
+	public APIParser(JenkinsVisitor visitor, String jobName,
+			String pluginUrlFragment) {
+		this.jobName = jobName;
+		this.pluginUrlFragment = pluginUrlFragment;
+
+		try {
+			json = (JSONObject) visitor.getJobReportData(this.jobName,
+					"lastSuccessfulBuild", pluginUrlFragment);
+		} catch (Exception e) {
+			logger.error("Error in visiting last successful build for job \""
+					+ jobName + "\". Details:\n"+StringUtils.getStrackTrace(e));
+		}
+
+		logger.info("Retrieving data from the last successful build of \""
+				+ this.jobName + "\".");
+	}
+
+	@Override
+	public String parse(List<String> dataItems) {
+		if (dataItems.size() > 0) {
+			data = new StringBuilder();
+
+			if (json != null) {
+
+				for (int i = 0; i < dataItems.size() - 1; i++) {
+					String dataItem = dataItems.get(i);
+
+					if (dataItem.indexOf(".") == -1) {
+						data.append(dataItem + ":")
+								.append(json == null ? "No such data" : json
+										.getString(dataItems.get(i)))
+								.append(",");
+
+					} else {
+						String result = getEmbeddedJSONValue(dataItem);
+						logger.info("Value of \"" + dataItem + "\" is:"
+								+ result);
+
+						data.append(dataItem + ":" + result + ",");
+					}
+
+				}
+
+				String dataItem = dataItems.get(dataItems.size() - 1);
+
+				if (dataItem.indexOf(".") == -1) {
+
+					data.append(dataItems.get(dataItems.size() - 1) + ":")
+							.append(json == null ? "No such data"
+									: json.getString(dataItems.get(dataItems
+											.size() - 1)));
+
+				} else {
+					String result = getEmbeddedJSONValue(dataItem);
+					logger.info("Value of \"" + dataItem + "\" is:" + result);
+
+					data.append(dataItem + ":" + result);
+				}
+
+			} else {
+				logger.error("All builds for job \"" + jobName
+						+ "\" have failed.");
+			}
+
+			return data.toString();
+
+		} else {
+			logger.error("No data items to parse for \""
+					+ this.pluginUrlFragment + "\" of \"" + this.jobName + "\"");
+
+			return "No data items to parse.";
+		}
+	}
+
+	private String getEmbeddedJSONValue(String dataItem) {
+		String[] items = dataItem.split("\\.");
+
+		JSONObject jsonObject = null;
+		String result = null;
+
+		if (items.length > 0) {
+
+			jsonObject = json.getJSONObject(items[0]);
+
+			for (int i = 1; i < items.length - 1; i++) {
+
+				if (jsonObject == null) {
+					result = dataItem + ":No such data,";
+					break;
+				} else {
+					jsonObject = jsonObject.getJSONObject(items[i]);
+				}
+			}
+
+			result = jsonObject.getString(items[items.length - 1]);
+
+		}
+
+		return result;
+	}
+}
